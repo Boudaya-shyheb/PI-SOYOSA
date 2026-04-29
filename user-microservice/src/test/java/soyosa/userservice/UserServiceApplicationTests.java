31296a43277be10375239ec8488a12f8b7169326
Oussema Haggui <ohaggui6@gmail.com>
2026-04-29 03:42:32 +0100

    some fixes

--- user-microservice/src/test/java/soyosa/userservice/UserServiceApplicationTests.java
+++ user-microservice/src/test/java/soyosa/userservice/UserServiceApplicationTests.java
@@ -3,9 +3,7 @@ package soyosa.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-})
+@SpringBootTest
 class UserServiceApplicationTests {
 
     @Test
